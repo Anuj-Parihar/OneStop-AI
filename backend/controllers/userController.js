358f218095c72c863734470283ec58c461a1adf1
Anuj-Parihar <anujparihar0001@gmail.com>
2025-09-03 18:49:01 +0530

    complete backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,3 +17,64 @@ export const getUserCreations = async (req, res) => {
         })
     }
 }
+
+
+export const getPublishedrCreations = async (req, res) => {
+    try {
+        const creations = await sql `SELECT * FROM creations WHERE publish = true ORDER BY created_at DESC`;
+        
+        res.json({
+            success: true,
+            creations
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const toggleLikeCreation = async (req, res) => {
+    try {
+        const {userId} = req.auth();
+        const {id} = req.body;
+
+        const [creation] = await sql `SELECT * FROM creations WHERE id = ${id}`;
+        
+        if(!creation){
+            return res.json({
+                success: false,
+                message: "Creation not found"
+            });
+        }
+
+        const currentLikes = creation.likes;
+        const userIdStr = userId.toString();
+        let updatedLikes;
+        let message;
+        if(currentLikes.includes(userIdStr)){
+            //user already liked the creation, so remove the like
+            updatedLikes = currentLikes.filter((user) => user !== userIdStr);
+            message = "Creastion unliked";
+        }else{
+            //user has not liked the creation, so add the like
+            updatedLikes = [...currentLikes, userIdStr];
+            message = "Creation liked";
+        }
+        
+        const formattedArray = `{${updatedLikes.json(',')}}`
+
+        await sql `UPDATE creations SET likes = ${formattedArray} :: text[] WHERE id = ${id}`;
+
+        res.json({
+            success: true,
+            message
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
